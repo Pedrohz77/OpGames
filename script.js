e5da362b24bcdea7624b872693cad28721af444d
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-20 17:09:54 -0300

    feat: Formluario API finalizado

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 const solicitarOrcamento = (event) => {
     let valorNome = document.getElementById("campo-nome").value
     let valorEmail = document.getElementById("campo-email").value
@@ -6,11 +13,32 @@ const solicitarOrcamento = (event) => {
     console.log(valorNome)
     console.log(valorEmail)
     console.log(valorDescricao)
-
+    
     let dadosForm = {
         nome: valorNome,
         email: valorEmail,
         descricao: valorDescricao
     }
-}
+    
+    fetch("http://localhost:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+    .then(resposta => {
+        console.log(resposta)
 
+        document.querySelector ("#cadastro form").reset()
+        
+        alert("Solicitação enviada")
+    })
+    .catch(erro => {
+        
+        console.error(erro)
+        alert("Erro desconhecido")
+    })
+
+    event.preventDefault()
+}
