4862113364f50b4099ccf400bba7c5c33a477a02
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-21 17:07:29 -0300

    feat: Menu Implementado

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
+// let menu = document.queryselector("#menu")
+let menu = document.getElementsByClassName("menu")[0]
+let iconesBarras = document.getElementById("icone-barras")
+let iconeX = document.querySelector("#icone-x")
+function abrirFecharMenu() {
+   
+    // Se o menu está fechado
+    if (menu.classList.contains("menu-fechado")) {
+        // Abri o menu
+        menu.classList.remove("menu-fechado")
 
+       // Mostrar icone X
+       iconeX.style.display = "inline"
+
+       // Esconder icone Barras
+       iconesBarras.style.display= "none"
+        
+    }
+    else {
+        // Fechar o menu
+        menu.classList.add("menu-fechado")
+        
+        // Mostrar icone X
+        iconeX.style.display = "none"
+
+        // Esconder icone Barras
+        iconesBarras.style.display= "inline"
+
+    }
+}
+
+window.onresize = () => {
+    menu.classList.remove("menu-fechado")
+    iconeX.style.display = "inline"
+    iconesBarras.style.display= "none"
+}
 
 const solicitarOrcamento = (event) => {
     let valorNome = document.getElementById("campo-nome").value
